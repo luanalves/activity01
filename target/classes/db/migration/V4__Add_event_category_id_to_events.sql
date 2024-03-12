67c6b6a5b4df255b7be06550293e7fcf94a2b66c
Luan alves <luan_alves@hotmail.com>
2024-03-11 22:14:41 -0300

    remove directory target from tracking

--- target/classes/db/migration/V4__Add_event_category_id_to_events.sql
+++
@@ -1,7 +0,0 @@
-ALTER TABLE events
-ADD COLUMN event_category_id INT,
-ADD CONSTRAINT fk_event_category
-    FOREIGN KEY (event_category_id)
-    REFERENCES events_categories(entity_id)
-    ON DELETE RESTRICT
-    ON UPDATE CASCADE;
